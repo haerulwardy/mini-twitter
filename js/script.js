1ef8794f6ae55b06c7f7b1a96dfdc9761cc7e79a
haerulwardy <heydicreative@gmail.com>
2021-04-07 07:54:09 +0700

    initialize files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// get btn-weet
+const btnTweet = document.querySelector('.btn-tweet');
+btnTweet.addEventListener('click', function() {
+	// get input value
+	const tweetInput = document.querySelector('.tweet-input');
+	const textTweet = document.getElementById('text-tweet');
+
+
+
+
+});
